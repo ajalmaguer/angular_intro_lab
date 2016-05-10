5b7374d71e5b0a7b0dab5d6d3d4140e4630b0315
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-09 17:41:37 -0700

    add kittie now works

--- js/KittiesController.js
+++ js/KittiesController.js
@@ -6,6 +6,7 @@ function KittiesController() {
   var vm = this
 
   vm.remove = remove
+  vm.addKittie = addKittie
 
   vm.allKitties = [
     {
@@ -38,9 +39,33 @@ function KittiesController() {
     }
   ]
 
+  vm.newKittie = {
+    catName: '',
+    monthsOld: null,
+    gender: '',
+    image: '',
+    adopted: false
+  }
+
   function remove (removeCat) {
     vm.allKitties = vm.allKitties.filter(function(cat){
       return cat.catName != removeCat.catName
     })
+  }
+
+  function addKittie() {
+    console.log("added kittie")
+    vm.allKitties.unshift({
+      catName: vm.newKittie.catName,
+      monthsOld: vm.newKittie.monthsOld,
+      gender: vm.newKittie.gender,
+      image: vm.newKittie.image,
+      adopted: false
+    })
+
+    vm.newKittie.catName = ""
+    vm.newKittie.monthsOld = null
+    vm.newKittie.gender = ""
+    vm.newKittie.image = ""
   }
 }
